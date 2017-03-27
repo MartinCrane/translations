436bb18d861704335ac54c281cdffc449393df92
MartinCrane <martin.crane@gmail.com>
2017-03-27 00:02:36 -0400

    bubble

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get 'shapes/float', to: 'shapes#float'
   post 'shapes/float', to: 'shapes#float_change'
 
+  get 'shapes/bubble', to: 'shapes#bubble'
+  # post 'shapes/bubble', to: 'shapes#bubble'
+
   get 'shapes/thirds', to: 'shapes#thirds'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
